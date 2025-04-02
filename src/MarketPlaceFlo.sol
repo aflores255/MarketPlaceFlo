f746854fac790ffab52a1ecf5726da4cbe5f9c16
Alberto <albertofm1992@gmail.com>
2025-04-02 22:27:15 +0300

    Modified README

---
+++ src/MarketPlaceFlo.sol
@@ -0,0 +1,23 @@
+//1. License
+//SPDX-License-Identifier: MIT
+
+//2. Solidity
+pragma solidity 0.8.28;
+
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+//3. Contract
+
+contract MarketPlaceFlo is Ownable{
+
+    constructor(address owner) Ownable(owner){
+
+    }
+
+//List NFT
+
+//Buy NFT
+
+//Cancel List
+
+}
